16287aed4cb9d7e1b25a599cfdd1da315d23a0d8
Jong9106 <juank_hr@hotmail.com>
2020-12-10 22:40:59 -0500

    if for module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from sys import argv as av
-ac = len(av) - 1
-i = 1
-if ac != 0 and ac != 1:
-    print("{:d} arguments:".format(ac))
-    for str in av[1:]:
-        print("{:d}: {}".format(i, str))
-        i += 1
-elif ac == 1:
-    print("{:d} argument:".format(ac))
-    for str in av[1:]:
-        print("{:d}: {}".format(i, str))
-else:
-    print("{:d} arguments.".format(ac))
+if __name__ == "__main__":
+    from sys import argv as av
+    ac = len(av) - 1
+    i = 1
+    if ac != 0 and ac != 1:
+        print("{:d} arguments:".format(ac))
+        for str in av[1:]:
+            print("{:d}: {}".format(i, str))
+            i += 1
+    elif ac == 1:
+        print("{:d} argument:".format(ac))
+        for str in av[1:]:
+            print("{:d}: {}".format(i, str))
+    else:
+        print("{:d} arguments.".format(ac))
