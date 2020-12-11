1d9f4915b962b2167ff3d5041fa9d3f9b1097387
Jong9106 <juank_hr@hotmail.com>
2020-12-10 22:35:22 -0500

    Program to print arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv as av
+ac = len(av) - 1
+i = 1
+if ac != 0 and ac != 1:
+    print("{:d} arguments:".format(ac))
+    for str in av[1:]:
+        print("{:d}: {}".format(i, str))
+        i += 1
+elif ac == 1:
+    print("{:d} argument:".format(ac))
+    for str in av[1:]:
+        print("{:d}: {}".format(i, str))
+else:
+    print("{:d} arguments.".format(ac))
