3dc37414d9a668f966b1361b8e6079c2a257d14c
Jong9106 <juank_hr@hotmail.com>
2020-12-15 16:58:16 -0500

    Change condition

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     cont = len(my_list)
-    if idx < 0 and cont < idx:
+    if idx < 0 or cont <= idx:
         return my_list
     for str in range(cont):
         if str == idx:
