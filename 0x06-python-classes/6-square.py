aa1ac36d07c89a00a443398eaae754130552d646
Jong9106 <juank_hr@hotmail.com>
2021-01-05 23:27:49 -0500

    raise error at the begining

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,6 +9,17 @@ class Square:
         self.__size = size
         self.__position = position
 
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if type(position) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
     def area(self):
         return self.__size ** 2
 
