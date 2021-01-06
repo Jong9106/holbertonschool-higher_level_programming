6ef1fb5f10a3c3a75e2a3207cebc2425f84a7029
Jong9106 <juank_hr@hotmail.com>
2021-01-05 23:59:11 -0500

    here the commit

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Program to define atribute size"""
+
+
+class Square:
+    """difine size attribute"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
