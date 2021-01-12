f19f118195a58b3c0992e03686184153330d1b07
Jong9106 <juank_hr@hotmail.com>
2021-01-11 23:03:26 -0500

    Change conditional at the begining

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,19 @@ class Rectangle:
     """ New private instance attribute width """
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
 
     @property
     def width(self):
