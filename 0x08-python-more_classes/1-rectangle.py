67c3a111059b92a19dc22a8af0014a1e52f92457
Jong9106 <juank_hr@hotmail.com>
2021-01-11 23:00:01 -0500

    private instance

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """ New private instance attribute width """
 
     def __init__(self, width=0, height=0):
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
 
     @property
     def width(self):
