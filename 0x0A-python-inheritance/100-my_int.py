ea8c2aa1233a71c64c484b219b612d2b4ebb5b18
Jong9106 <juank_hr@hotmail.com>
2021-01-19 23:41:57 -0500

    New class for int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module to define a new class """
+
+
+class MyInt(int):
+    """ A class MyInt that inherits from int """
+    
+    def __eq__(self, op):
+        return self.value != op
+
+    def __ne__(self, op):
+        return self.value == op
