44f0db9266356ebef950d050cf9a3650b7cd34b0
Jong9106 <juank_hr@hotmail.com>
2021-01-20 12:58:40 -0500

    Function to read a file and print it

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module to create a function """
+
+
+def read_file(filename=""):
+    """ Function to read a file and print it in stdout """
+
+    with open(filename) as f:
+        for line in f:
+            print(line, end="")
+        print()
