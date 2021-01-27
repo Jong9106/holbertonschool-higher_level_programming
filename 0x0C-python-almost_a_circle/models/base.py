ec754a948d9e105c94ef8c49be39357744ca6b41
Jong9106 <juank_hr@hotmail.com>
2021-01-26 22:56:46 -0500

    Added json str representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,6 +10,17 @@ class Base:
     def __init__(self, id=None):
         self.id = id
 
-        if id == None:
+        if id is None:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ static method to return a json string representation """
+
+        import json
+
+        if list_dictionaries is None or not list_dictionaries:
+            return json.dumps([])
+
+        return json.dumps(list_dictionaries)
