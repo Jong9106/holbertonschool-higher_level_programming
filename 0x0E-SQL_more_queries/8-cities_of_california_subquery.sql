78e375b5cc36667b7f9c9784ee367edeba6a7b3c
Jong9106 <juank_hr@hotmail.com>
2021-03-04 10:32:29 -0500

    Added new line

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,5 @@
 -- script that lists all the cities of California that can be found in the database
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM cities WHERE name ="California")
+SELECT id, name FROM cities 
+-- Compare id 
+WHERE state_id = (SELECT id FROM cities WHERE name ="California")
 ORDER BY id ASC;
