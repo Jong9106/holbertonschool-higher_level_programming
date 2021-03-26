7f3a656d342da1cd0d8f7077e9ea392fe8d25153
Jong9106 <juank_hr@hotmail.com>
2021-03-25 23:52:06 -0500

    delete old file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
-const file1 = fs.readFileSync(process.argv[2], 'utf8');
-const file2 = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], file1 + file2);
+const dict = require('./101-data').dict;
+
+const newdict = {};
+for (const uid in dict) {
+  if (newdict[dict[uid]] === undefined) {
+    newdict[dict[uid]] = [];
+  }
+  newdict[dict[uid]].push(uid);
+}
+console.log(newdict);
