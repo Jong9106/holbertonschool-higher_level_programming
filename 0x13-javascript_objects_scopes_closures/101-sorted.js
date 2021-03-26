d34353df6797ca6fe51cf8721425481d9a23bbf3
Jong9106 <juank_hr@hotmail.com>
2021-03-25 23:48:23 -0500

    delte encoding var

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const data1 = fs.readFileSync(process.argv[2], { encoding: 'utf8', flag: 'r' });
-const data2 = fs.readFileSync(process.argv[3], { encoding: 'utf8', flag: 'r' });
-fs.writeFileSync(process.argv[4], data1 + data2);
+const file1 = fs.readFileSync(process.argv[2], 'utf8');
+const file2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], file1 + file2);
