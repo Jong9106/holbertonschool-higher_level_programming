679daf4c26c2bdf644697d917f6f085e9443c5be
Jong9106 <juank_hr@hotmail.com>
2021-03-28 22:52:53 -0500

    Pep8

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module that define a new class """ 
+""" Module that define a new class """
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
