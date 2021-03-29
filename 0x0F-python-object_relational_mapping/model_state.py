b8c16ae7b2433ee9f508f835cf7ded2b106bd2f1
Jong9106 <juank_hr@hotmail.com>
2021-03-28 22:52:26 -0500

    new module that define a State class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module that define a new class """ 
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    """ New class thta define State than inherits from Base """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
