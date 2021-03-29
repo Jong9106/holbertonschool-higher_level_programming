833c38d6b370187f01f914cdd35bdbce5dec7f67
Jong9106 <juank_hr@hotmail.com>
2021-03-28 19:44:35 -0500

    change format

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" script that takes in an argument and displays all values """
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    data_base = sys.argv[3]
+    state_name = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=user_name,
+        passwd=password, db=data_base, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s ",(state_name,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
