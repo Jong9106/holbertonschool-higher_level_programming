5e4df57ea0baad27a1d2fa13918ee280a84e5801
Jong9106 <juank_hr@hotmail.com>
2021-04-11 00:14:00 -0500

    change comments

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET
+# script that sends a request and display status code of the response.
 curl -so /dev/null -w "%{http_code}" "$1"
