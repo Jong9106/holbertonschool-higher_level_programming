1c123bce6aa101ce9b6cc4d904d3c83c7928d493
Jong9106 <juank_hr@hotmail.com>
2021-04-11 23:13:54 -0500

    Fixed variable name

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@ import requests
 
 if __name__ == "__main__":
     url = requests.get('https://intranet.hbtn.io/status')
-    rsp = response.text
+    rsp = url.text
     print('Body response:')
-    print('\t- type: {}'.format(type(resp)))
-    print('\t- content: {}'.format(url))
+    print('\t- type: {}'.format(type(rsp)))
+    print('\t- content: {}'.format(rsp))
