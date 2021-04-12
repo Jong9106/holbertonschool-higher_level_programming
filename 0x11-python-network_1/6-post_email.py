3978e1e5f5e20fdd959b2445cd9037051653d261
Jong9106 <juank_hr@hotmail.com>
2021-04-11 23:53:23 -0500

    fixed print

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -13,4 +13,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {'email': email}
-    response = requests.post
+    response = requests.post(url, data=data)
+    print(response.text)
