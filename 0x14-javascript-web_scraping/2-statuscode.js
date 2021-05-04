4241345524c3a2d3ca71317ad52afe2f06c8c272
Jong9106 <juank_hr@hotmail.com>
2021-05-04 18:42:15 -0500

    Get request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+const request = require('request');
+
+request(argv[2], function (error, response) {
+  if (error) {
+    console.error('code:', error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
