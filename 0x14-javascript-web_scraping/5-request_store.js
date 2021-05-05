a62ed29726a33b4beb34172b043145f3154d6999
Jong9106 <juank_hr@hotmail.com>
2021-05-04 23:04:07 -0500

    import lorem ipsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+const request = require('request');
+const fs = require('fs');
+const url = argv[2];
+const fileName = argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    fs.writeFile(fileName, data, (err) =>{
+      if (err) throw err;
+    });
+  }
+});
